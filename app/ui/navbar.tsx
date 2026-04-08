6ba6cbc871bd09b2a74ad997490188e1f76b4e71
solomon-hackett <solomonhackett2008@gmail.com>
2026-04-08 17:38:21 +0100

    continued working on the navbar

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -12,17 +12,20 @@ const links = [
 
 export default function NavBar() {
   return (
-    <nav className="flex flex-row justify-between items-center mt-2 px-5 py-3 rounded-full w-full liquid-glass">
+    <nav className="top-2 right-0 left-0 z-10 fixed flex justify-between items-center backdrop-blur-md mx-2 px-5 rounded-4xl h-20 liquid-glass">
       <Image
         src="/logo.png"
         width="1047"
         height="530"
         alt="BSDC logo with hacker aesthetic"
-        className="w-40"
+        className="w-30"
       />
-      <div aria-label="Links">
+      <div
+        aria-label="Links"
+        className="flex gap-5 px-5 py-3 rounded-xl liquid-glass"
+      >
         {links.map((link) => (
-          <Link key={link.name} href={link.href}>
+          <Link key={link.name} href={link.href} className="text-3xl">
             {link.name}
           </Link>
         ))}
--- app/ui/page-heading.tsx
+++ app/ui/page-heading.tsx
@@ -3,7 +3,7 @@ import { press_start } from "./fonts";
 export default function PageHeading({ heading }: { heading: string }) {
   return (
     <h1
-      className={`px-10 py-5 rounded-3xl text-green-300 text-7xl liquid-glass text-center mt-10
+      className={`px-10 py-5 rounded-3xl text-green-300 text-7xl liquid-glass text-center mt-5 w-fit mb-5
     ${press_start.className} `}
     >
       {heading}
